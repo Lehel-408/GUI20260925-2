8540e89470a53fc4f121a44287be9508bc18abf9
szocs.lehel <szocs.lehel@24-DIAK7>
2026-09-25 14:00:02 +0200

    Second commit (unfinished)

--- CalculatorApp/Form1.cs
+++ CalculatorApp/Form1.cs
@@ -5,6 +5,24 @@ namespace CalculatorApp
         public Calculator()
         {
             InitializeComponent();
+            int num1 = txtBoxNum1.Text != "" ? int.Parse(txtBoxNum1.Text) : 0;
+            int num2 = txtBoxNum2.Text != "" ? int.Parse(txtBoxNum2.Text) : 0;
+            btnSum.Click += BtnSum_Click;
+            btnSub.Click += BtnSub_Click;
+            btnMult.Click += BtnMult_Click;
+            btnDiv.Click += BtnDiv_Click;
+            btnDelete.Click += BtnDelete_Click;
+        }
+
+        private void BtnSum_Click(object? sender, EventArgs e)
+        {
+            double result = num1 + num2;
+            txtBoxEredmeny.Text = result.ToString();
+        }
+
+        private void BtnDelete_Click(object? sender, EventArgs e)
+        {
+            txtBoxEredmeny.Text = "";
         }
     }
 }
